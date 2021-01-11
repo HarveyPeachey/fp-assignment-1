51c9b72d0ce86252e6427ab90e8e984d1b3fa1ed
harveypeachey <harveypeachey@hotmail.co.uk>
2021-01-11 20:59:29 +0000

    added map solution and renamed function with timing tests

--- src/assignment_1/task_1.clj
+++ src/assignment_1/task_1.clj
@@ -1,11 +1,16 @@
 (ns assignment-1.task_1 "Task - Squaring lists")
 
 
-(defn squareList [v]
+(defn square-list-recur [v]
   (loop [values v sqValues []]
     (if (empty? values)
       sqValues
       (recur (rest values) (conj sqValues (#(* % %) (first values)))))))
 
+(defn square-list-map [v]
+  (map #(* % %) v))
 
-(squareList [4 4 2 1 1 43 123 123 223 1 55 23 123 545 12 765])
+
+
+(time (square-list-recur [4 4 2 1 1 43 123 123 223 1 55 23 123 545 12 765]))
+(time (square-list-map [4 4 2 1 1 43 123 123 223 1 55 23 123 545 12 765]))
