77f904747bc61216cc8293b9a6149e71a4257c96
HarveyPeachey <harveypeachey@hotmail.co.uk>
2021-01-11 18:56:15 +0000

    task 1 with recursion

--- src/assignment_1/task_1.clj
+++ src/assignment_1/task_1.clj
@@ -1,1 +1,11 @@
-(ns assignment-1.task_1)
+(ns assignment-1.task_1 "Task - Squaring lists")
+
+
+(defn squareList [v]
+  (loop [values v sqValues []]
+    (if (empty? values)
+      sqValues
+      (recur (rest values) (conj sqValues (#(* % %) (first values)))))))
+
+
+(squareList [4 4 2 1 1])
