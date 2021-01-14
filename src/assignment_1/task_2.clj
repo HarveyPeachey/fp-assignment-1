3c9ce408ea15a27ad7d36e9b14354012ac2acb2d
HarveyPeachey <harveypeachey@hotmail.co.uk>
2021-01-14 19:46:19 +0000

    Start of task 2

--- src/assignment_1/task_2.clj
+++ src/assignment_1/task_2.clj
@@ -1,1 +1,8 @@
-(ns assignment-1.task_2)
+(ns assignment-1.task_2 "Task 2 - Change Counter")
+
+(defn change-counter [amount]
+  (let [coins [1 5 10 25]]
+    (loop [c coins combinations 0]
+      (if (empty? c)
+       combinations
+       (recur)))))
