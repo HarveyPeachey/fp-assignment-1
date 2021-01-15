98216138a46ba626eb766cba69c2db864410c7ff
harveypeachey <harveypeachey@hotmail.co.uk>
2021-01-15 22:38:22 +0000

    added unit tests for find-plants and format-garden functions

--- test/assignment_1/task_3_test.clj
+++ test/assignment_1/task_3_test.clj
@@ -1,3 +1,22 @@
 (ns assignment-1.task_3-test
   (:require [clojure.test :refer :all]
             [assignment-1.task_3 :refer :all]))
+
+(deftest find-plants-test
+  (testing "Finding plants"
+    (testing "when called with"
+      (testing "a childs name as a string"
+        (is (= ["Clover" "Grass" "Grass" "Clover"] (find-plants "Ginny")))
+        (is (= ["Violet" "Violet"] (find-plants "Alice" "VVVVV")) "and a correct garden format")
+        (is (= "Uh oh, that's a not a garden..." (find-plants "Alice" '("VVV"))) "and an incorrect garden format")))))
+
+(deftest format-garden-test
+  (testing "Garden formatter"
+    (testing "when called with"
+      (testing "a string length of 24"
+        (is (= [["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W"]]
+               (format-garden "ABCDEFGHIJKLMNOPQRSTUVW"))))
+      (testing "a string of more than 24"
+        (is (= [["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W"]
+                ["Y" "Z"]]
+               (format-garden "ABCDEFGHIJKLMNOPQRSTUVWXYZ")))))))
