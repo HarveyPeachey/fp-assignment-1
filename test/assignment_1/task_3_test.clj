a1e9289595a745c0f092e59e219b698eba773843
harveypeachey <harveypeachey@hotmail.co.uk>
2021-01-15 23:20:09 +0000

    added find-childs-plants unit test

--- test/assignment_1/task_3_test.clj
+++ test/assignment_1/task_3_test.clj
@@ -6,9 +6,12 @@
   (testing "Finding plants"
     (testing "when called with"
       (testing "a childs name as a string"
-        (is (= ["Clover" "Grass" "Grass" "Clover"] (find-plants "Ginny")))
-        (is (= ["Violet" "Violet"] (find-plants "Alice" "VVVVV")) "and a correct garden format")
-        (is (= "Uh oh, that's a not a garden..." (find-plants "Alice" '("VVV"))) "and an incorrect garden format")))))
+        (is (= ["Clover" "Grass" "Grass" "Clover"]
+               (find-plants "Ginny")))
+        (is (= ["Violet" "Violet"]
+               (find-plants "Alice" "VVVVV")) "and a correct garden format")
+        (is (= "Uh oh, that's a not a garden..."
+               (find-plants "Alice" '("VVV"))) "and an incorrect garden format")))))
 
 (deftest format-garden-test
   (testing "Garden formatter"
@@ -20,3 +23,23 @@
         (is (= [["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W"]
                 ["Y" "Z"]]
                (format-garden "ABCDEFGHIJKLMNOPQRSTUVWXYZ")))))))
+
+(deftest find-childs-plants
+  (let [garden  [[\V \R \C \G \V \V \R \V \C \G \G \C \C \G \V \R \G \C \V \C \G \C \G \V]]
+                [\V \R \C \C \C \G \C \R \R \G \V \C \G \C \R \V \V \C \V \G \C \G \C \V]
+        garden2 [[\V \R \C \G \V \V \R \V \C \G \G \C \C \G \V \R \G \C \V \C \G \C \G \V]]
+                [\V \R \C \C \C \G \C \R \R \G \V \C \G \C \R \V \V \C \V \G \C \G \C \V]
+                [\G]])
+  (testing "Finding a childs plants"
+    (testing "when called with"
+      (testing "a given child string"
+        (testing "that has a default garden of 48 plants"
+          (is (= ["Violet" "Radish" "Violet" "Radish"]
+                 (find-childs-plants "Alice" garden))  "exists in the class")
+          (is (= "Sorry that child can't afford plants"
+                 (find-childs-plants "Harvey" garden)) "doesn't exist in the class"))
+        (testing "that has a obscure garden that's added an extra row"
+          (is (= ["Violet" "Radish" "Violet" "Radish" "Grass"]
+                 (find-childs-plants "Alice" garden)) "with a child who has a plant on it"
+           (is (= ["Clover" "Grass" "Clover" "Clover"]
+                 (find-childs-plants "Bob" garden)) "with a child who hasn't got a plant on it")))))))
