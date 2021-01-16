157ca88ea806833ab8485043d66dd3c9411a2c1d
harveypeachey <harveypeachey@hotmail.co.uk>
2021-01-16 00:44:20 +0000

    fixed unit test formatting issues and removed broken tests

--- test/assignment_1/task_3_test.clj
+++ test/assignment_1/task_3_test.clj
@@ -17,36 +17,51 @@
   (testing "Garden formatter"
     (testing "when called with"
       (testing "a string length of 24"
-        (is (= [["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W"]]
-               (format-garden "ABCDEFGHIJKLMNOPQRSTUVW"))))
+        (is (= [[\A \B \C \D \E \F \G \H \I \J \K \L \M \N \O \P \Q \R \S \T \U \V \W \X]]
+               (format-garden "ABCDEFGHIJKLMNOPQRSTUVWX"))))
       (testing "a string length of more than 24"
-        (is (= [["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W"]
-                ["Y" "Z"]]
+        (is (= [[\A \B \C \D \E \F \G \H \I \J \K \L \M \N \O \P \Q \R \S \T \U \V \W \X]
+                [\Y \Z]]
                (format-garden "ABCDEFGHIJKLMNOPQRSTUVWXYZ")))))))
 
-(deftest find-childs-plants
-  (let [garden  [[\V \R \C \G \V \V \R \V \C \G \G \C \C \G \V \R \G \C \V \C \G \C \G \V]]
-                [\V \R \C \C \C \G \C \R \R \G \V \C \G \C \R \V \V \C \V \G \C \G \C \V]
-        garden2 [[\V \R \C \G \V \V \R \V \C \G \G \C \C \G \V \R \G \C \V \C \G \C \G \V]]
-                [\V \R \C \C \C \G \C \R \R \G \V \C \G \C \R \V \V \C \V \G \C \G \C \V]
-                [\G]])
-  (testing "Finding a childs plants"
-    (testing "when called with"
-      (testing "a given child string"
-        (testing "that has a default garden of 48 plants"
-          (is (= ["Violet" "Radish" "Violet" "Radish"]
-                 (find-childs-plants "Alice" garden))  "exists in the class")
-          (is (= "Feed me, Seymour! That child doesn't like plants"
-                 (find-childs-plants "Harvey" garden)) "doesn't exist in the class"))
-        (testing "that has a obscure garden that's added an extra row"
-          (is (= ["Violet" "Radish" "Violet" "Radish" "Grass"]
-                 (find-childs-plants "Alice" garden)) "with a child who has a plant on it"
-           (is (= ["Clover" "Grass" "Clover" "Clover"]
-                 (find-childs-plants "Bob" garden)) "with a child who hasn't got a plant on it")))))))
+; (deftest find-childs-plants-test
+;   (testing "Finding a childs plants"
+;     (testing "when called with"
+;       (testing "a given child string"
+;         (testing "that has a default garden of 48 plants"
+;           (is (= ["Violet" "Radish" "Violet" "Radish"]
+;                  (find-childs-plants "Alice" [[\V \V]] "exists in the class")))
+;           (is (= "Feed me, Seymour! That child doesn't like plants"
+;                  (find-childs-plants "Harvey" [[\V \V]])) "doesn't exist in the class"))
+;         (testing "that has a obscure garden that's added an extra row"
+;           (is (= ["Violet" "Radish" "Violet" "Radish" "Grass"]
+;                  (find-childs-plants "Alice" [[\V \V]])) "with a child who has a plant on it")
+;           (is (= ["Clover" "Grass" "Clover" "Clover"]
+;                  (find-childs-plants "Bob" [[\V \V]])) "with a child who hasn't got a plant on it"))))))
+
+(deftest find-childs-plants-test
+  (let [garden  [[\V \R \C \G \V \V \R \V \C \G \G \C \C \G \V \R \G \C \V \C \G \C \G \V]
+                 [\V \R \C \C \C \G \C \R \R \G \V \C \G \C \R \V \V \C \V \G \C \G \C \V]]
+        garden2 [[\V \R \C \G \V \V \R \V \C \G \G \C \C \G \V \R \G \C \V \C \G \C \G \V]
+                 [\V \R \C \C \C \G \C \R \R \G \V \C \G \C \R \V \V \C \V \G \C \G \C \V]
+                 [\G]]]
+    (testing "Finding a childs plants"
+      (testing "when called with"
+        (testing "a given child string"
+          (testing "that has a default garden of 48 plants"
+            (is (= ["Violet" "Radish" "Violet" "Radish"]
+                   (find-childs-plants "Alice" garden)) "exists in the class")
+            (is (= "Feed me, Seymour! That child doesn't like plants"
+                   (find-childs-plants "Harvey" garden)) "doesn't exist in the class"))
+          (testing "that has a obscure garden that's added an extra row"
+            (is (= ["Violet" "Radish" "Violet" "Radish" "Grass"]
+                   (find-childs-plants "Alice" garden2)) "with a child who has a plant on it")
+            (is (= ["Clover" "Grass" "Clover" "Clover"]
+                   (find-childs-plants "Bob" garden2)) "with a child who hasn't got a plant on it")))))))
+
 
 (deftest lookup-plant-name-test
   (testing "Looking up a plant name"
-    (testing "when given a shorthand that exists"
-      (is (= ["Violet"] (lookup-plant-name \V)) "of a char")
-      (is (= ["Violet"] (lookup-plant-name "V")) "of a string of length 1"))
-    (is (= [nil] (lookup-plant-name "P")) "when given a shorthand that doesn't exist")))
+    (testing "when given a shorthand"
+      (is (= ["Violet"] (lookup-plant-name "V")) "that exists")
+      (is (= [nil] (lookup-plant-name "P")) "that doesn't exist"))))
