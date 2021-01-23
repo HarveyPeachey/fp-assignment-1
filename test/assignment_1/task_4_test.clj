0abc6aeb8fb24f54696ddaa2b07835dac142dea4
HarveyPeachey <harveypeachey@hotmail.co.uk>
2021-01-23 23:41:25 +0000

    finished unit tests for task-4, took the right piss

--- test/assignment_1/task_4_test.clj
+++ test/assignment_1/task_4_test.clj
@@ -5,9 +5,9 @@
 (deftest get-data-test
   (testing "Retrieving data with the get-data function"
     (is (= "caught exception: java.io.FileNotFoundException: https://www.metoffice.gov.uk/hadobs/hadcet/cetdl1772on.da"
-           (get-data "https://www.metoffice.gov.uk/hadobs/hadcet/cetdl1772on.da" 14 #"\s+")) "With an invalid slurp url")
+           (get-data "https://www.metoffice.gov.uk/hadobs/hadcet/cetdl1772on.da" 14 #"\s+")) "with an invalid slurp url")
     (is (= [1772 1 32 -15 18 25 87 128 187 177 105 111 78 112]
-           (first (get-data)))) "With the valid default slurp url"))
+           (first (get-data)))) "with the valid default slurp url"))
 
 (deftest make-weather-record-test
   (testing "Creating a weather data record"
@@ -76,12 +76,127 @@
       (get-formatted-data '(["foo"]))
       (get-formatted-data '(1772 1 32 -15 18 25 87 128 187 177 105 111 78 112)))))
 
-(deftest lookup-month-name-test)
+(deftest lookup-month-name-test
+  (testing "Month name lookup function"
+    (is (= "January"
+           (lookup-month-name 0)) "with a valid month number")
+    (is (= nil
+           (lookup-month-name -1))) "with an invalid month number"))
 
-(deftest average-test)
+(deftest average-test
+  (testing "Average mean calculation when given a collection"
+    (is (= 4
+           (average [2 4 6])))))
 
-(deftest smallest-variation-test)
+(deftest smallest-variation-test
+  (testing "Smallest variation calculation when given a collection that each contain a key of temperature"
+    (is (= '({:temperature 4})
+           (smallest-variation 5 '({:temperature 1}
+                                   {:temperature 2}
+                                   {:temperature 3}
+                                   {:temperature 4}
+                                   {:temperature 7}))))))
 
-(deftest greatest-variation-test)
+(deftest greatest-variation-test
+  (testing "Greatest variation calculation when given a collection that each contain a key of temperature"
+    (is (= '({:temperature 1})
+           (greatest-variation 5 '({:temperature 1}
+                                   {:temperature 2}
+                                   {:temperature 3}
+                                   {:temperature 4}
+                                   {:temperature 7}))))))
 
-(deftest task4-data-assertions)
+(def formatted-data-fixture '({:day 1, :month 1, :year 1772, :temperature 323.0}
+                              {:day 29, :month 2, :year 1772, :temperature -151.0}
+                              {:day 29, :month 2, :year 1776, :temperature 185.0}
+                              {:day 1, :month 1, :year 2001, :temperature 252.0}
+                              {:day 1, :month 5, :year 2002, :temperature 875.0}
+                              {:day 1, :month 6, :year 1772, :temperature 1281.0}
+                              {:day 1, :month 6, :year 1772, :temperature 1872.0}
+                              {:day 1, :month 8, :year 1772, :temperature 1773.0}
+                              {:day 1, :month 9, :year 1772, :temperature 1054.0}
+                              {:day 1, :month 10, :year 1772, :temperature 1151.0}))
+
+(def weather-data-year-fixture '({:year 1772, :temperature 1043.2857142857142}
+                                 {:year 1776, :temperature 185.0}
+                                 {:year 2001, :temperature 252.0}
+                                 {:year 2002, :temperature 875.0}))
+
+(def weather-data-month-fixture '({:month 1, :temperature 287.5}
+                                  {:month 2, :temperature 17.0}
+                                  {:month 5, :temperature 875.0}
+                                  {:month 6, :temperature 1576.5}
+                                  {:month 8, :temperature 1773.0}
+                                  {:month 9, :temperature 1054.0}
+                                  {:month 10, :temperature 1151.0}))
+
+(def weather-data-month-year-fixture '({:month 9, :year 1772, :temperature 1054.0}
+                                       {:month 2, :year 1772, :temperature -151.0}
+                                       {:month 6, :year 1772, :temperature 1576.5}
+                                       {:month 1, :year 1772, :temperature 323.0}
+                                       {:month 8, :year 1772, :temperature 1773.0}
+                                       {:month 5, :year 2002, :temperature 875.0}
+                                       {:month 1, :year 2001, :temperature 252.0}
+                                       {:month 2, :year 1776, :temperature 185.0}
+                                       {:month 10, :year 1772, :temperature 1151.0}))
+
+
+(deftest task4-questions-functions-test
+  (testing "Each function to answer task 4's questions when passed with dummy formatted data"
+    (is (= '("January 1 1772 323.0"
+             "February 29 1776 185.0"
+             "May 1 2002 875.0"
+             "June 1 1772 1872.0"
+             "August 1 1772 1773.0"
+             "September 1 1772 1054.0"
+             "October 1 1772 1151.0")
+           (warmest-day-each-month formatted-data-fixture)) "for the warmest day each month")
+    (is (= '({:year 1772, :temperature 1043.2857142857142}
+             {:year 1776, :temperature 185.0}
+             {:year 2001, :temperature 252.0}
+             {:year 2002, :temperature 875.0})
+           (mean-temp-each-year formatted-data-fixture)) "for the mean temperature for each year")
+    (is (= "Warmest year was 1772 at a temperature of 1043.2857142857142 and Coldest year was 1776 at a temperature of 185.0"
+           (warmest-and-coldest-year weather-data-year-fixture)) "for the warmest and coldest years")
+    (is (= '({:month 1, :temperature 287.5}
+             {:month 2, :temperature 17.0}
+             {:month 5, :temperature 875.0}
+             {:month 6, :temperature 1576.5}
+             {:month 8, :temperature 1773.0}
+             {:month 9, :temperature 1054.0}
+             {:month 10, :temperature 1151.0})
+           (mean-temp-each-month formatted-data-fixture)) "for the mean temperature for each month")
+    (is (= '({:month 9, :year 1772, :temperature 1054.0}
+             {:month 2, :year 1772, :temperature -151.0}
+             {:month 6, :year 1772, :temperature 1576.5}
+             {:month 1, :year 1772, :temperature 323.0}
+             {:month 8, :year 1772, :temperature 1773.0}
+             {:month 5, :year 2002, :temperature 875.0}
+             {:month 1, :year 2001, :temperature 252.0}
+             {:month 2, :year 1776, :temperature 185.0}
+             {:month 10, :year 1772, :temperature 1151.0})
+           (mean-temp-each-month-year formatted-data-fixture)) "for the mean temperature for each month for each year")
+    (is (= '("Variation against January for Greatest: 1772 and Smallest: 1772"
+             "Variation against February for Greatest: 1772 and Smallest: 1772"
+             "Variation against May for Greatest: 2002 and Smallest: 2002"
+             "Variation against June for Greatest: 1772 and Smallest: 1772"
+             "Variation against August for Greatest: 1772 and Smallest: 1772"
+             "Variation against September for Greatest: 1772 and Smallest: 1772"
+             "Variation against October for Greatest: 1772 and Smallest: 1772")
+           (greatest-and-smallest-variation weather-data-month-year-fixture
+                                            weather-data-month-fixture)) "for the greatest and smallest variation against each month")
+    (is (= "Warmest leap day was in 1776 at a temperature of 185.0"
+           (warmest-leap-day formatted-data-fixture)) "for the warmest leap day")
+    (is (= '({:century 18, :temperature 936.0}
+             {:century 21, :temperature 563.5})
+           (mean-temp-each-century formatted-data-fixture)) "for the mean temperature for each century"))
+  (testing "that is invalid"
+    (are [x] (thrown? AssertionError x)
+      (warmest-day-each-month '({:foo 1.0}))
+      (mean-temp-each-year '({:foo 1.0}))
+      (warmest-and-coldest-year '({:foo 1.0}))
+      (mean-temp-each-month '({:foo 1.0}))
+      (mean-temp-each-month-year '({:foo 1.0}))
+      (greatest-and-smallest-variation '({:foo 1.0}) '({:foo 1.0}))
+      (warmest-leap-day '({:foo 1.0}))
+      (mean-temp-each-century '({:foo 1.0})))))
